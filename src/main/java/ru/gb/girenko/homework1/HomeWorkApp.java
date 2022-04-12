2253d134883113da66384e532a5a0b27ed3bec63
MikhailGirenko <misha15girenko@gmail.com>
2022-04-12 10:42:40 +0300

    2 task

--- src/main/java/ru/gb/girenko/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/girenko/homework1/HomeWorkApp.java
@@ -1,7 +1,12 @@
 package ru.gb.girenko.homework1;
 
 public class HomeWorkApp {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
     public static void main(String[] args) {
-
+        printThreeWords();
     }
 }
