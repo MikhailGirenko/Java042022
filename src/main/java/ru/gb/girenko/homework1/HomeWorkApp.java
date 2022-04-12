018d55c5f557341e442756f04ad3b968ecdee9cd
MikhailGirenko <misha15girenko@gmail.com>
2022-04-12 11:07:53 +0300

    4 task

--- src/main/java/ru/gb/girenko/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/girenko/homework1/HomeWorkApp.java
@@ -15,8 +15,19 @@ public class HomeWorkApp {
             System.out.println("Сумма отрицательная");
         }
     }
+    public static void printColor(){
+        int value = 100;
+        if (value<=0) {
+            System.out.println("Красный");
+        } else if (value>0 && value<=100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        printColor();
     }
 }
