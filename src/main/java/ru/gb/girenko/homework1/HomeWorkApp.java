ecd97d4f5b634fcfda1b7d2086a8984feade3333
MikhailGirenko <misha15girenko@gmail.com>
2022-04-12 10:53:35 +0300

    3 task

--- src/main/java/ru/gb/girenko/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/girenko/homework1/HomeWorkApp.java
@@ -6,7 +6,17 @@ public class HomeWorkApp {
         System.out.println("Banana");
         System.out.println("Apple");
     }
+    public static void checkSumSign(){
+        int a = 2;
+        int b = -3;
+        if (a+b>=0) {
+            System.out.println("Сумма положительная");
+        } else{
+            System.out.println("Сумма отрицательная");
+        }
+    }
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 }
