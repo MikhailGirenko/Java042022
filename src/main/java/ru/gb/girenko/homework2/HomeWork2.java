29267b70c0c631280743849696e3f9c329c763ea
MikhailGirenko <misha15girenko@gmail.com>
2022-04-15 10:04:28 +0300

    5 task

--- src/main/java/ru/gb/girenko/homework2/HomeWork2.java
+++ src/main/java/ru/gb/girenko/homework2/HomeWork2.java
@@ -1,11 +1,14 @@
 package ru.gb.girenko.homework2;
 
+import java.time.Year;
+
 public class HomeWork2 {
     public static void main(String[] args) {
         System.out.println(firstTask(16,5));
         System.out.println(secondTask(-3));
         System.out.println(thirdTask(-10));
         fourthTask(4,"Hello!");
+        System.out.println(fifthTask(1992));
     }
     public static boolean firstTask(int a, int b ){
         if (a+b >= 10 && a+b <= 20){
@@ -32,5 +35,12 @@ public class HomeWork2 {
        for(int i=0; i<a; i++){
            System.out.println(text);
        }
+    }
+    public static boolean fifthTask(int year ){
+        if((year%4==0 && year%100!=0)|| year%400==0){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
