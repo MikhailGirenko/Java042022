c2a5b1b5e4911d4ba0b9324ca5c898bc5d90b40d
MikhailGirenko <misha15girenko@gmail.com>
2022-04-15 09:05:11 +0300

    2 task

--- src/main/java/ru/gb/girenko/homework2/HomeWork2.java
+++ src/main/java/ru/gb/girenko/homework2/HomeWork2.java
@@ -3,12 +3,20 @@ package ru.gb.girenko.homework2;
 public class HomeWork2 {
     public static void main(String[] args) {
         System.out.println(firstTask(16,5));
+        System.out.println(secondTask(-3));
     }
     public static boolean firstTask(int a, int b ){
         if (a+b >= 10 && a+b <= 20){
             return true;
         }else {
             return false;
+        }
+    }
+    public static String secondTask(int a){
+        if(a>=0){
+            return "Число положительное!";
+        }else {
+            return "Число отрицательное!";
         }
     }
 }
