53135da0bd2d3e7bdcd7d9b7fcfb20876ab4ba81
MikhailGirenko <misha15girenko@gmail.com>
2022-04-19 09:56:23 +0300

    2 task

--- src/main/java/ru/gb/girenko/homework3/Homework3.java
+++ src/main/java/ru/gb/girenko/homework3/Homework3.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class Homework3 {
     public static void main(String[] args) {
        arrayZeroOne();
+       System.out.println();
+       array100();
     }
     public static void arrayZeroOne() {
         int [] nums = {0,0,0,1,0,1,0,1,0,1,0,1,1};
@@ -17,5 +19,13 @@ public class Homework3 {
             }
             System.out.print(nums[i] + ", ");
         }
-        }
     }
+
+    public static void array100() {
+        int[] arr = new int[100];
+        for(int i = 0, j=1; i<arr.length;i++, j++){
+            arr[i]=j;
+            }
+        System.out.print(Arrays.toString(arr));
+    }
+}
