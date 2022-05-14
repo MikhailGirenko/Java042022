a48732f0de5a6f0ac7f3069d8f8ecc6b765bbdc8
MikhailGirenko <misha15girenko@gmail.com>
2022-05-14 21:52:48 +0300

    The main task.

---
+++ src/main/java/ru/gb/girenko/homework7/Plate.java
@@ -0,0 +1,14 @@
+package ru.gb.girenko.homework7;
+
+public class Plate {
+    private int food;
+    public Plate(int food){
+        this.food=food;
+    }
+    public void info(){
+        System.out.println("Plate: "+food);
+    }
+    public void decreaseFood(int n){
+        food-=n;
+    }
+}
