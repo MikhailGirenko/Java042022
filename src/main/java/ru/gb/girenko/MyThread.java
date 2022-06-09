40f903da5f34421078a0886233896ac2d54633f9
MikhailGirenko <misha15girenko@gmail.com>
2022-06-09 19:32:04 +0300

    Task 1

---
+++ src/main/java/ru/gb/girenko/MyThread.java
@@ -0,0 +1,20 @@
+package ru.gb.girenko;
+
+public class MyThread extends Thread{
+    private float [] arr;
+
+    public MyThread(float[] arr) {
+        this.arr = arr;
+    }
+
+    public float[] getArr() {
+        return arr;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=(float) (arr[i]*Math.sin(0.2f+i/5)*Math.cos(0.4f+i/2));
+        }
+    }
+}
